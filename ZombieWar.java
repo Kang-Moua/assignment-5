9939182c41336fb7d49905e9b2ccd24c3936bef4
Kengen1196 <Nguyenk10@csp.edu>
2021-11-30 16:07:31 -0600

    Update ZombieWar.java

--- ZombieWar.java
+++ ZombieWar.java
@@ -1,296 +1,213 @@
 import java.util.Random;
+import java.util.ArrayList;
 
 public class ZombieWar {
     public static void main(String[] args) {
-        Survivor[] survivors = generateSurvivors();
-        Zombie[] zombies = generateZombies();
-
-        printInitialMessage(survivors, zombies);
-
-        while(survivors.length > numDeadSurvivors(survivors) && zombies.length > numDeadZombies(zombies)) {
-            attackSequence(survivors, zombies);
-        }
-
-        printFinalSurvivorCount(survivors);
-    }
-
-    static Survivor[] generateSurvivors(){
         Random rand = new Random();
-        int randSurvivors = rand.nextInt(9) + 1; // +1 to prevent zero bound (1-10 survivors)
-        Survivor[] survivors = new Survivor[randSurvivors];
+        int randSurvivors = rand.nextInt(20) + 1; // +1 to prevent zero bound (1-10 survivors)
         int numSoldiers = rand.nextInt(randSurvivors);
         int numTeachers = rand.nextInt(randSurvivors - numSoldiers);
         int numChildren = randSurvivors - numSoldiers - numTeachers;
 
-        int counter = 0;
+        int numZombies = rand.nextInt(20) + 1;
+        int numCommonInfected = rand.nextInt(numZombies);
+        int numTanks = numZombies - numCommonInfected;
+
+        ArrayList<Survivor> survivors = generateSurvivors(numSoldiers, numTeachers, numChildren);
+        ArrayList<Zombie> zombies = generateZombies(numCommonInfected, numTanks);
+
+        printInitialMessage(survivors, zombies, numSoldiers, numTeachers, numChildren, numCommonInfected, numTanks);
+
+        while (!survivors.isEmpty() && !zombies.isEmpty()) {
+            attackSequence(survivors, zombies);
+        }
+        printFinalSurvivorCount(survivors);
+    }
+
+    static ArrayList<Survivor> generateSurvivors(int numSoldiers, int numTeachers, int numChildren) {
+        ArrayList<Survivor> survivors = new ArrayList<Survivor>();
         for (int x = 0; x < numSoldiers; x++) {
-            survivors[counter] = new Soldier();
-            counter++;
+            survivors.add(new Soldier(x + 1));
         }
         for (int x = 0; x < numTeachers; x++) {
-            survivors[counter] = new Teacher();
-            counter++;
+            survivors.add(new Teacher(x + 1));
         }
         for (int x = 0; x < numChildren; x++) {
-            survivors[counter] = new Child();
-            counter++;
+            survivors.add(new Child(x + 1));
         }
 
         return survivors;
     }
 
-    static Zombie[] generateZombies(){
-        Random rand = new Random();
-        int numZombies = rand.nextInt(9) + 1;
-        Zombie[] zombies = new Zombie[numZombies];
-        int numCommonInfected = rand.nextInt(numZombies);
-        int numTanks = numZombies - numCommonInfected;
-
-        int counter = 0;
+    static ArrayList<Zombie> generateZombies(int numCommonInfected, int numTanks) {
+        ArrayList<Zombie> zombies = new ArrayList<Zombie>();
         for (int x = 0; x < numCommonInfected; x++) {
-            zombies[counter] = new CommonInfected();
-            counter++;
+            zombies.add(new CommonInfected(x + 1));
         }
         for (int x = 0; x < numTanks; x++) {
-            zombies[counter] = new Tank();
-            counter++;
+            zombies.add(new Tank(x + 1));
         }
 
         return zombies;
     }
 
-    static int getNumSurvivors(Survivor[] survivors) {
-        return survivors.length;
-    }
-
-    static int getNumZombies(Zombie[] zombies) {
-        return zombies.length;
-    }
-
-    static void attackSequence(Survivor[] survivors, Zombie[] zombies) {
-        for (Survivor survivor : survivors) {
-            if(!survivor.isDead) { // Check if survivor is alive, only attack if not dead
-                for (Zombie zombie : zombies) {
-                    if (!zombie.isDead) {
-                        survivor.attack(zombie, survivor.getAttack()); // Survivors attack zombies first
-                        zombie.isDead(zombie.getHealth()); // Check/set if zombie is dead by checking if health is less than zero
-                    }
-                }
+    static void attackSequence(ArrayList<Survivor> survivors, ArrayList<Zombie> zombies) {
+        for (int j = 0; j < survivors.size(); j++) {
+            for (int i = 0; i < zombies.size(); i++) {
+                survivors.get(j).attack(zombies.get(i)); // Survivors attack zombies first
+                if (zombies.get(i).isDead() == true) {
+                    System.out.println(survivors.get(j).getName() + " killed " + zombies.get(i).getName());
+                    zombies.remove(i);
+                    i--;
+                } // Check/set if zombie is dead by checking if health is less
+                  // than zero
             }
         }
 
-        for (Zombie zombie : zombies) {
-            if(!zombie.isDead) { // Check if zombie is alive, only attack if not dead
-                for (Survivor survivor : survivors) {
-                    if (!survivor.isDead) {
-                        zombie.attack(survivor, zombie.getAttack()); // Zombies attack survivors
-                        survivor.isDead(survivor.getHealth()); // Check/set if survivor is dead by checking if health is less than zero
-                    }
-                }
+        for (int j = 0; j < zombies.size(); j++) {
+            for (int i = 0; i < survivors.size(); i++) {
+                zombies.get(j).attack(survivors.get(i)); // Zombies attack survivors
+                if (survivors.get(i).isDead()) {
+                    System.out.println(zombies.get(j).getName() + " killed " + survivors.get(i).getName());
+                    survivors.remove(i);
+                    i--;
+                } // Check/set if survivor is dead by checking if health is
+                  // less than zero
             }
         }
     }
 
-    static int numDeadSurvivors(Survivor[] survivors) {
-        int numDeadSurvivors = 0;
+    static void printInitialMessage(ArrayList<Survivor> survivors, ArrayList<Zombie> zombies, int numSoldiers,
+            int numTeachers, int numChildren, int numCommonInfected, int numTanks) {
+        int numSurvivors = survivors.size();
+        int numZombies = zombies.size();
 
-        for(Survivor survivor : survivors) {
-            if (survivor.isDead) {
-                numDeadSurvivors++;
-            }
-        }
-        return numDeadSurvivors;
+        System.out.println("We have " + numSurvivors + " survivors trying to make it to safety. (" + numChildren
+                + " children, " + numTeachers + " teachers, " + numSoldiers + " soldiers)");
+        System.out.println("But there are " + numZombies + " zombies waiting for them. (" + numCommonInfected
+                + " common infected, " + numTanks + " tanks)");
     }
 
-    static int numDeadZombies(Zombie[] zombies) {
-        int numDeadZombies = 0;
-
-        for(Zombie zombie : zombies) {
-            if (zombie.isDead) {
-                numDeadZombies++;
-            }
-        }
-        return numDeadZombies;
-    }
-
-    static void printInitialMessage(Survivor[] survivors, Zombie[] zombies) {
-        int numSurvivors = getNumSurvivors(survivors);
-        int numZombies = getNumZombies(zombies);
-
-        System.out.println("We have " + numSurvivors + " survivors trying to make it to safety.");
-        System.out.println("But there are " + numZombies + " zombies waiting for them.");
-    }
-
-    static void printFinalSurvivorCount(Survivor[] survivors) {
-        if(survivors.length == numDeadSurvivors(survivors)) {
+    static void printFinalSurvivorCount(ArrayList<Survivor> survivors) {
+        if (survivors.size() <= 0) {
             System.out.println("None of the survivors made it.");
-        }
-        else {
-            System.out.println("It seems " + (survivors.length - numDeadSurvivors(survivors)) + " have made it to safety.");
+        } else {
+            System.out.println(
+                    "It seems " + (survivors.size()) + " have made it to safety.");
         }
     }
 }
 
 abstract class Survivor {
-    int health;
-    int attack;
-    boolean isDead;
+    private int id;
+    private int health;
+    private int attack;
+    String type;
 
-    public int getHealth() {
-        return this.health;
+    public Survivor(int id, int health, int attack, String type) {
+        this.id = id;
+        this.health = health;
+        this.attack = attack;
+        this.type = type;
+    }
+
+    public int getID() {
+        return id;
     }
 
     public void setHealth(int attack) {
         this.health = health - attack;
     }
 
-    int getAttack(){
-        return attack;
-    }
-
-    void attack(Zombie zombie, int attack) {
+    public void attack(Zombie zombie) {
         zombie.setHealth(attack);
     }
 
-    void isDead(int health) {
+    public boolean isDead() {
         if (health <= 0) {
-            isDead = true;
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    public String getName() {
+        return type + " " + id;
     }
 }
 
 class Soldier extends Survivor {
-    int health = 100;
-    int attack = 10;
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int attack) {
-        this.health = health - attack;
-    }
-
-    @Override
-    public int getAttack() {
-        return attack;
-    }
-
-    void attack(Zombie zombie, int attack) {
-        zombie.setHealth(attack);
+    public Soldier(int id) {
+        super(id, 100, 10, "Soldier");
     }
 }
 
 class Teacher extends Survivor {
-    int health = 50;
-    int attack = 5;
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int attack) {
-        this.health = health - attack;
-    }
-
-    @Override
-    public int getAttack() {
-        return attack;
-    }
-
-    void attack(Zombie zombie, int attack) {
-        zombie.setHealth(attack);
+    public Teacher(int id) {
+        super(id, 50, 5, "Teacher");
     }
 }
 
 class Child extends Survivor {
-    int health = 20;
-    int attack = 2;
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int attack) {
-        this.health = health - attack;
-    }
-
-    @Override
-    public int getAttack() {
-        return attack;
-    }
-
-    void attack(Zombie zombie, int attack) {
-        zombie.setHealth(attack);
+    public Child(int id) {
+        super(id, 20, 2, "Child");
     }
 }
 
 abstract class Zombie {
+    int id;
     int health;
     int attack;
-    boolean isDead;
+    String type;
+
+    public Zombie(int id, int health, int attack, String type) {
+        this.id = id;
+        this.health = health;
+        this.attack = attack;
+        this.type = type;
+    }
 
     public int getHealth() {
         return this.health;
+    }
+
+    public int getID() {
+        return id;
     }
 
     public void setHealth(int attack) {
         this.health = health - attack;
     }
 
-    int getAttack(){
+    int getAttack() {
         return attack;
     }
 
-    void attack(Survivor survivor, int attack) {
+    void attack(Survivor survivor) {
         survivor.setHealth(attack);
     }
 
-    public void isDead(int health) {
+    public boolean isDead() {
         if (health <= 0) {
-            isDead = true;
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    public String getName() {
+        return type + " " + id;
     }
 }
 
 class CommonInfected extends Zombie {
-    int health = 30;
-    int attack = 5;
-
-    public int getHealth() {
-        return this.health;
-    }
-
-    public void setHealth(int attack) {
-        this.health = health - attack;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    void attack(Survivor survivor, int attack) {
-        survivor.setHealth(attack);
+    public CommonInfected(int id) {
+        super(id, 30, 5, "Common Infected");
     }
 }
 
 class Tank extends Zombie {
-    int health = 150;
-    int attack = 20;
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int attack) {
-        this.health = health - attack;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    void attack(Survivor survivor, int attack) {
-        survivor.setHealth(attack);
+    public Tank(int id) {
+        super(id, 150, 20, "Tank");
     }
 }
